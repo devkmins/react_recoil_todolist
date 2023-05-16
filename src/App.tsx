cd6fca88c6303aad7cf4fef6fb0f2609c46446f6
Min <esspacetoM@gmail.com>
2023-05-16 20:45:27 +0900

    #6 TO-DO

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import { createGlobalStyle } from "styled-components";
-import ToDoList from "./ToDoList";
+import ToDoList from "./components/ToDoList";
 
 const GlobalStyle = createGlobalStyle`
 @import url('https://fonts.googleapis.com/css2?family=Open+Sans:wght@400;500&family=Source+Sans+Pro:wght@300;400&display=swap');
